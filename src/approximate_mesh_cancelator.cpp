88791715327147ede4e6e6844a75446da8b06cd1
kolya <kolya46592@gmail.com>
2023-09-28 11:13:58 -0400

    Added some clears and comments

--- src/approximate_mesh_cancelator.cpp
+++ src/approximate_mesh_cancelator.cpp
@@ -146,8 +146,10 @@ bool ApproximateMeshCancelator::add_particle(BankedParticle& p) {
 Make 2 different algorithms:
 
 First for both, create vector<int> keys
-this represents the keys which are in the sparse matrix that have bins with 1 or more particles in them across all nodes
-we do this by going through all nodes and sending to master every bin id that has a particle in it. Afterward, send keys vector to all nodes.
+this represents the keys which are in the sparse matrix that have bins with 1 or
+more particles in them across all nodes we do this by going through all nodes
+and sending to master every bin id that has a particle in it. Afterward, send
+keys vector to all nodes.
 
 Algo 1:
 Loop through each key which is a bin with particles in it on at least one node
@@ -167,7 +169,7 @@ Vector<double> wgts;
 vector<double> wgts2;
 vector<uint16> N+;
 vector<uint16> N-;
-vector<uin16> N2+;  
+vector<uin16> N2+;
 vector<uint16> N2-;
 
 All these vectors should have the same size which is keys.size()
@@ -180,62 +182,57 @@ ReduceAllSum these two arrays using data_vector
 
 */
 
+void ApproximateMeshCancelator::sync_keys(std::vector<int>& keys) {
 
-  void ApproximateMeshCancelator::sync_keys(std::vector<int>& keys)
-{
-  for (auto& key_bin_pair : bins) 
-    {
-      keys.push_back(key_bin_pair.first);
-    }
-    std::set<int> key_set;
-    if(mpi::rank == 0)
-    {
-      std::copy(keys.begin(),keys.end(),std::inserter(key_set,key_set.end()));
+  // Each node collects all keys 
+  for (auto& key_bin_pair : bins) {
+    keys.push_back(key_bin_pair.first);
+  }
+  std::set<int> key_set;
+
+  // Put master keys into the keyset
+  if (mpi::rank == 0) {
+    std::copy(keys.begin(), keys.end(), std::inserter(key_set, key_set.end()));
+    keys.clear();
+    keys.shrink_to_fit();
+  }
+
+  // For every Node starting at 1, send its keys to master and add to key_set
+  for (int i = 1; i < mpi::size; i++) {
+    if (mpi::rank == i) {
+      mpi::Send(keys, 0);
       keys.clear();
-      keys.shrink_to_fit(); 
+      keys.shrink_to_fit();
+    } else if (mpi::rank == 0) {
+      mpi::Recv(keys, i);
+      std::copy(keys.begin(), keys.end(),
+                std::inserter(key_set, key_set.end()));
     }
-    
-    for(int i = 1; i < mpi::size;i++)
-    {
-      if(mpi::rank == i)
-      {
-        mpi::Send(keys,0);
-        keys.clear();
-      }
-      else if (mpi::rank == 0)
-      {
-        mpi::Recv(keys,i);
-        std::copy(keys.begin(),keys.end(),std::inserter(key_set,key_set.end()));
-      }
+  }
+
+  // Move keys from key_set back to keys in master
+  if (mpi::rank == 0) {
+    keys.clear();
+    keys.assign(key_set.begin(), key_set.end());
+    keys.shrink_to_fit();
+    key_set.clear();
+  }
+
+  // Send keys to all nodes from Master
+  for (int i = 1; i < mpi::size; i++) {
+    if (mpi::rank == 0) {
+      mpi::Send(keys, i);
+    } else if (mpi::rank == i) {
+      mpi::Recv(keys, 0);
     }
-    if(mpi::rank == 0)
-    {
-      keys.clear();
-      keys.assign(key_set.begin(),key_set.end());
-       
-    }
-    for(int i = 1; i < mpi::size;i++)
-    {
-      if(mpi::rank == 0)
-      {
-        mpi::Send(keys,i);
-      }
-      else if (mpi::rank == i)
-      {
-        mpi::Recv(keys,0);
-        sleep(i + 1);
-       
-      }
-    }
+  }
 }
 
-
 void ApproximateMeshCancelator::perform_cancellation_loop(pcg32& /*rng*/) {
-
   std::vector<int> keys;
   keys.reserve(bins.size());
   sync_keys(keys);
-    
+
 }
 void ApproximateMeshCancelator::perform_cancellation(pcg32& /*rng*/) {
   // Go through all bins in the mesh
