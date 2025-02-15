12ce80e39e548666559324f51d7a2d0569b5a8c5
Parth Singh <148287414+2018uch0002@users.noreply.github.com>
2025-02-15 18:49:10 -0500

    Update include/simulation/collision_operators/keff_fission_operator.hpp
    Co-authored-by: Hunter Belanger <hunter.belanger@gmail.com>

--- include/simulation/collision_operators/keff_fission_operator.hpp
+++ include/simulation/collision_operators/keff_fission_operator.hpp
@@ -47,9 +47,9 @@ class KeffFissionOperator
     // keff of the previous generation, so that the number of particles per
     // generation stays approximately constant.
 
-    // When the fission source is converged (i.e., after inactive genertaions),
-    // k-avg can be used. Though, after the first active generation, the k-avg
-    // will be recored, otherwise k from the generation can be used.
+    // When the fission source is converged (i.e., after inactive generations),
+    // kcol-avg can be used to improve stability. However, kcol-avg is only
+    // non-zero AFTER the first active generation, hence the condition.
     if (Tallies::instance().generations() > 1) {
       return static_cast<int>(std::floor(
           std::abs(k_abs_scr) / Tallies::instance().kcol_avg() + p.rng()));
